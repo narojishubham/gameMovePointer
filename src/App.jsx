410171e6f319e53f9c4d6b4c23107734f2e5f8c2
shubham.naroji <shubham.naroji@infi-pre.com>
2024-03-08 13:46:32 +0530

    slower animation

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,15 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import "./App.css";
 import video1 from "./video/video.gif";
 
 function App() {
   const [position, setPosition] = useState({ x: 0, y: 0 });
-  const [isMoving, setIsMoving] = useState(false);
 
   const handleClick = (event) => {
     const x = event.clientX;
     const y = event.clientY;
 
     setPosition({ x, y });
-    setIsMoving(true);
-
-    // Reset the moving state after the animation duration
-    setTimeout(() => {
-      setIsMoving(false);
-    }, 1000); // Adjust the duration as needed
   };
 
   return (
@@ -25,11 +18,8 @@ function App() {
       style={{ height: "100vh", width: "100vw" }}
       onClick={handleClick}
     >
-      <div
-        className={`logo ${isMoving ? "moving" : ""}`}
-        style={{ left: position.x, top: position.y }}
-      >
-        <img src={video1} width="100" height="100" alt="video1" />
+      <div className="logo" style={{ left: position.x, top: position.y }}>
+        <img src={video1} width="100" height="100" alt="video" />
       </div>
     </div>
   );
